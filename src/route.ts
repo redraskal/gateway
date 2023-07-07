3e4bad810dca027e5bff7cd48538b88baf4810e4
redraskal <ben@ryben.dev>
2023-07-06 20:47:35 -0500

    feat: json Route method

--- src/route.ts
+++ src/route.ts
@@ -12,6 +12,8 @@ export interface Route {
 	body?<T extends Route>(data: Data<T>, err?: Error): RouteResponse;
 };
 
+const acceptedJSONMethods = ["POST", "PUT", "PATCH", "DELETE"];
+
 export class Route {
 	/**
 	 * Read the body from the Request as a {@link FormData} object.
@@ -19,11 +21,25 @@ export class Route {
 	 * This first decodes the data from UTF-8, then parses it as a
 	 * `multipart/form-data` body or a `application/x-www-form-urlencoded` body.
 	 *
+	 * @param req Request
 	 * @returns Promise<FormData> - The body of the request as a {@link FormData}.
 	 */
 	static async formData(req: Request) {
 		if (req.method == "POST") {
 			return await req.formData();
+		}
+		return null;
+	}
+
+	/**
+	 * Read the body from the Request as a JSON object.
+	 * 
+	 * @param req Request
+	 * @returns Promise<T> - JSON object
+	 */
+	static async json<T>(req: Request) {
+		if (req.headers.get("accept") == "application/json" && acceptedJSONMethods.includes(req.method)) {
+			return await req.json<T>();
 		}
 		return null;
 	}
