5fa1e89375e35b10b07ff82a0ef515c2bbd7fa73
redraskal <ben@ryben.dev>
2023-07-06 21:10:39 -0500

    feat: parse html templates inside of html templates

--- src/html.ts
+++ src/html.ts
@@ -10,7 +10,13 @@ export default function (strings: TemplateStringsArray, ...values: unknown[]): H
 	let html = "";
 	for (let i = 0; i < strings.length; i++) {
 		if (i > 0) {
-			html += values[i - 1];
+			let value = values[i - 1];
+			let asHTML = value as HTMLTemplateString;
+			if (asHTML.value) {
+				html += asHTML.value;
+			} else {
+				html += value;
+			}
 		}
 		html += strings[i].replaceAll(/[\n\t]/g, "");
 	}
