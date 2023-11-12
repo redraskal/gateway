61f9bfcf80f8340f4ecd03ee43d1985e01d9e973
redraskal <ben@ryben.dev>
2023-11-11 23:33:53 -0600

    chore: remove bash requirement

--- bin.sh
+++ bin.sh
@@ -1,14 +1,15 @@
-#!/usr/bin/env bash
-if [[ "$1" == "dev" ]]; then
-	while true; do
-		GATEWAY_ENV=dev bun --hot node_modules/gateway/src/index.ts
-		exit_code=$?
-		if [ $exit_code -ne 8 ]; then
-			exit
-		fi
-	done
-elif [[ "$1" == "gen" ]]; then
-	GATEWAY_GEN=$2 bun node_modules/gateway/src/index.ts
+#!/bin/sh
+
+if [ "$1" = "dev" ]; then
+    while true; do
+        GATEWAY_ENV=dev bun --hot node_modules/gateway/src/index.ts
+        exit_code=$?
+        if [ "$exit_code" -ne 8 ]; then
+            exit
+        fi
+    done
+elif [ "$1" = "gen" ]; then
+    GATEWAY_GEN="$2" bun node_modules/gateway/src/index.ts
 else
-	bun node_modules/gateway/src/index.ts
+    bun node_modules/gateway/src/index.ts
 fi
